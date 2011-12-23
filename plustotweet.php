035f98bbb9707b0a42c29ed11e3b9f154ed5dd63
Jeffrey Lensen <jeffrey@hyves.nl>
2011-12-23 11:41:59 +0100

    Switched to tmhOAuth. Added functionality to upload images from a G+ post to Twitter.

--- plustotweet.php
+++ plustotweet.php
@@ -6,7 +6,7 @@
  * 
  */
 
-require_once('twitteroauth/twitteroauth.php');
+require_once('tmhOAuth/tmhOAuth.php');
 
 $google_userid = "<your_google_userid>";
 $google_key = "<your_google_api_key>";
@@ -16,21 +16,30 @@ $twitter_consumerSecret = "<your_twitter_consumerSecret>";
 $twitter_oauthToken = "<your_twitter_oauthToken>";
 $twitter_oauthSecret = "<your_twitter_oauthSecret>";
 
-$tweet = new TwitterOAuth($twitter_consumerKey, $twitter_consumerSecret, $twitter_oauthToken, $twitter_oauthSecret);
+$tmhOAuth = new tmhOAuth(array(
+  'consumer_key'    => $twitter_consumerKey,
+  'consumer_secret' => $twitter_consumerSecret,
+  'user_token'      => $twitter_oauthToken,
+  'user_secret'     => $twitter_oauthSecret,
+));
 
 $max_results = 5;
-$plus_filter = "&fields=items(title,published,url,verb)";
+$plus_filter = "items(title,verb,url,published,object/attachments(objectType,content,fullImage/url))";
 $checktime = (int)(date('U', time()));
+$imagename = "upload.jpg";
 
 while (true) {
-    $request = @file_get_contents("https://www.googleapis.com/plus/v1/people/$google_userid/activities/public?key=$google_key&maxResults=$max_results$plus_filter");
+    $request = @file_get_contents("https://www.googleapis.com/plus/v1/people/$google_userid/activities/public?key=$google_key&maxResults=$max_results&fields=$plus_filter");
     $obj = json_decode($request);
 
     echo "Previous check: ".date('r', $checktime)."\n";
     foreach ($obj->{'items'} as $item) {
+        @unlink($imagename);
+        $image = False;
         $title = $item->{'title'};
         $verb = $item->{'verb'};
         $url = $item->{'url'};
+        $attachments = (empty($item->{'object'}->{'attachments'})) ? array() : $item->{'object'}->{'attachments'};
         $published = $date = date('U', strtotime($item->{'published'}));
 
         if ($published < $checktime) {
@@ -39,9 +48,33 @@ while (true) {
         else {
             if ($title != "" and $verb != "share" ) {
                 $shorturl = @file_get_contents("http://hyv.es/api/?url=$url");
-                echo "Title: $title\nURL: $shorturl\n";
-                echo "Posting to Twitter\n";
-                $tweet->post('statuses/update', array('status' => "$title $shorturl"));
+                foreach ($attachments as $attachment) {
+                    $a_type = $attachment->{'objectType'};
+                    $a_content = $attachment->{'content'};
+                    $a_url = $attachment->{'fullImage'}->{'url'};
+                    if ( $a_type == "photo" and $a_content != "" ) {
+                        $image = True;
+                        file_put_contents($imagename, file_get_contents($a_url));
+                        break;
+                    }
+                }
+                $status = "$title $shorturl";
+                echo "Posting to Twitter: $status\n";
+                if ($image) {
+                    $tmhOAuth->request('POST', 'https://upload.twitter.com/1/statuses/update_with_media.json',
+                        array(
+                            'media[]'  => "@{$imagename}",
+                            'status'   => $status
+                        ),
+                        true, // use auth
+                        true  // multipart
+                    );
+                }
+                else {
+                     $tmhOAuth->request('POST', $tmhOAuth->url('1/statuses/update'), array(
+                        'status' => $status
+                    ));
+                }
             }
         }
     }
